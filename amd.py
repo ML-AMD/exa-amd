66aa4f2e26fdb858a8cae5e1fdcc5aaec7da6272
Maxim Moraru <moraru@ch-fe2.lanl.gov>
2024-11-05 10:28:32 -0700

    add the vasp workflow

--- amd.py
+++ amd.py
@@ -1,34 +1,50 @@
 import sys
 from parsl_configs.chicoma import chicoma_config
-from parsl_tasks.dft_optimization import vasp_relaxation
+from parsl_tasks.dft_optimization import run_vasp_calc
 import parsl
 import time
 import os
-
+import json
+    
 parsl.load(chicoma_config)
 
 def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
 
 if __name__ == '__main__':
-    work_dir="/users/moraru/Parsl-Project/exa-amd/work_dir"
-    work_subdir_prefix="tmp"
+    
+    config_file_name = "/users/moraru/Parsl-Project/exa-amd/configs/std_config.json"
+    with open(config_file_name, "r") as file:
+        config = json.load(file)
+
+    output_file_vasp_calc = config["output_file_name"]
+    work_dir = config["work_dir"]
+    work_subdir_prefix = "work_subdir"
     nb_of_dft_calculations = 4
     
     start_dft_calc = time.time()
+    # launch the tasks (all the vasp calculations)
     l_futures = []
-    for i in range(1, nb_of_dft_calculations+1):
+    for i in range( 204, nb_of_dft_calculations+204):
         work_subdir = os.path.join(work_dir,"{}_{}".format(work_subdir_prefix,i))
         if not os.path.exists(work_subdir):
             os.makedirs(work_subdir)
-            l_futures.append(vasp_relaxation(work_subdir, i))
+            l_futures.append(run_vasp_calc(config_file_name, work_subdir, i))
         else:
             eprint("work_dir ({}) already exists".format(work_dir))
-        
+            sys.exit(1)
+            
+    # open the output file to log the structures that failed or succeded to converge
+    fp = open(output_file_vasp_calc, 'w')
+    fp.write("id,result\n")
+    
+    # wait for all the tasks to complete
     for future in l_futures:
         try:
-            future.result()
-        except parsl.app.errors.BashExitFailure as e:
+            id, result = future.result()
+            fp.write("{},{}\n".format(id,result))
+            
+        except Exception as e:
             eprint("Exception: ", e)
     
     end_dft_calc = time.time()
--- parsl_tasks/dft_optimization.py
+++ parsl_tasks/dft_optimization.py
@@ -1,19 +1,101 @@
-from parsl import python_app, bash_app
+from parsl import python_app, bash_app, join_app
+from tools.errors import VaspNonReached
+
+@python_app
+def vasp_timeout(id):
+    return (id, "time_out")
+
+@python_app
+def vasp_non_reached(id):
+    return (id, "non_reached")
+
+@python_app
+def vasp_success(id):
+    return (id, "success")
+
+@python_app
+def bash_exit_failure(id):
+    return (id, "bash_exit_failure")
+
 
 @bash_app(executors=['chicoma_single_gpu_per_worker'])
-def vasp_relaxation(work_dir, id, ):
+def vasp_relaxation(config_file_name, work_dir, id, walltime=(int)):
     import os 
     import shutil
+    import json
     os.chdir(work_dir)
-
-    output_file = os.path.join(work_dir,"output_{}.rx".format(id))
-    vasp_std_exe = "/usr/projects/icapt/applications/vasp/vasp-6.4.2-nvidia-gpu/bin/vasp_std"
-    poscar = os.path.join("/users/moraru/Parsl-Project/data/select_structures/new", "POSCAR_{}".format(id))
-    incar = "/users/moraru/Parsl-Project/CMS/ctest/INCAR.rx"
-    potcar = "/users/moraru/Parsl-Project/data/POTCAR"    
+    with open(config_file_name, "r") as file:
+        config = json.load(file)
     
+    output_file = os.path.join(work_dir,"output_{}.rx".format(id))
+    vasp_std_exe = config["vasp_std_exe"]
+    poscar = os.path.join(config["selected_structures_dir"], "POSCAR_{}".format(id))
+    incar = config["incar_rx"]
+    potcar = config["potcar"] 
+    
+    # relaxation
     shutil.copy(poscar, os.path.join(work_dir, "POSCAR"))
     shutil.copy(incar, os.path.join(work_dir, "INCAR"))
     shutil.copy(potcar, os.path.join(work_dir,"POTCAR"))
     
-    return "timeout 10 $PARSL_SRUN_PREFIX {} > {} ".format(vasp_std_exe, output_file)
+    return "$PARSL_SRUN_PREFIX {} > {} ".format(vasp_std_exe, output_file)
+
+
+@bash_app(executors=['chicoma_single_gpu_per_worker'])
+def vasp_energy_calculation(config_file_name, work_dir, id, walltime=(int)):
+    import os 
+    import shutil
+    import json
+    output_rx = os.path.join(work_dir,"output_{}.rx".format(id))
+    
+    # grep "reached"
+    reached = False
+    with open(output_rx, "r") as file:
+        for line in file:
+            if "reached" in line:
+                reached = True
+                break
+    
+    if not reached:
+        raise VaspNonReached  
+
+    os.chdir(work_dir)
+    with open(config_file_name, "r") as file:
+        config = json.load(file)
+        
+    vasp_std_exe = config["vasp_std_exe"]
+    incar_en = config["incar_en"]
+    output_file = os.path.join(work_dir,"output_{}.en".format(id))
+    os.rename("OUTCAR", "OUTCAR_{}.rx".format(id))
+    shutil.copy("CONTCAR", "CONTCAR_{}".format(id))
+    shutil.copy("CONTCAR", "POSCAR")
+    shutil.copy(incar_en, "INCAR")
+    
+    return "$PARSL_SRUN_PREFIX {} > {} ".format(vasp_std_exe, output_file)
+    
+@join_app()
+def run_vasp_calc(config_file_name, work_dir, id):
+    from parsl.app.errors import AppTimeout
+    f_relax: Future = vasp_relaxation(config_file_name, work_dir, id, walltime=36)
+    try:
+        f_relax.result()
+    except AppTimeout as e:
+        return vasp_timeout(id)
+    except parsl.app.errors.BashExitFailure as e:
+        return bash_exit_failure(id)
+    except Exception as e:
+        raise e 
+    
+    f_energy: Future = vasp_energy_calculation(config_file_name, work_dir, id, walltime=36)
+    try:
+        f_energy.result()
+    except VaspNonReached:
+        return vasp_non_reached(id)
+    except AppTimeout as e:
+        return vasp_timeout(id)
+    except parsl.app.errors.BashExitFailure as e:
+        return bash_exit_failure(id)
+    except Exception as e:
+        raise e
+    
+    return vasp_success(id)
